9eb339751bea1722c8a1a75ebab51c0cfe52fa1e
danielhuamani <danielhuamani15@gmail.com>
2023-04-05 10:46:08 -0500

    added config serverless

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import serverless_wsgi
 from flask import Flask, jsonify, make_response, request
 import pdfkit
 import boto3
@@ -5,12 +6,19 @@ import os
 from .services import ContractUpdate
 from .utils import generate_random_letter, date_string
 
-s3_resource = boto3.resource('s3', aws_access_key_id=os.environ.get('AWS_ID'),
-    aws_secret_access_key=os.environ.get('AWS_KEY'))
-
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=os.environ.get('AWS_ID'),
+    aws_secret_access_key=os.environ.get('AWS_KEY'),
+    config=boto3.session.Config(signature_version='s3v4', region_name=os.environ.get('REGION'))
+)
 BUCKET = 'finniu-contracts'
 AWS_S3 = f"https://{BUCKET}.s3.us-east-2.amazonaws.com/"
 app = Flask(__name__)
+LAMBDA_TASK_ROOT = os.environ.get('LAMBDA_TASK_ROOT', os.path.dirname(os.path.abspath(__file__)))
+
+WKHTMLTOPDF_PATH = '/opt/bin/wkhtmltopdf'
+config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_PATH)
 
 
 @app.route("/build-pdf", methods=['POST'])
@@ -21,14 +29,19 @@ def build_pdf():
         'encoding': 'UTF-8',
         'enable-local-file-access': True
     }
-    pdf_value = pdfkit.from_string(html, False, options=options)
+    print("configuration", WKHTMLTOPDF_PATH)
+    pdf_value = pdfkit.from_string(html, False, options=options, configuration=config)
     uuid = data.get("uuid")
     filename = data.get("filename")
     random_str = generate_random_letter()
     filename_key = f"{date_string()}/{filename}-{uuid}-{random_str}.pdf"
     file_url = f"{AWS_S3}{filename_key}"
-    s3_resource.Bucket(BUCKET).put_object(
-        Key=filename_key, Body=pdf_value, ContentType="application/pdf", ACL='public-read')
+    print('filename_key', filename_key, file_url)
+    s3_client.put_object(Body=pdf_value, Bucket=BUCKET, ContentType="application/pdf", Key=filename_key, ACL='public-read')
+    print("upload")
     ContractUpdate.execute(uuid, file_url)
     return jsonify(message='success', url=file_url)
 
+
+def handler(event, context):
+    return serverless_wsgi.handle_request(app, event, context)
