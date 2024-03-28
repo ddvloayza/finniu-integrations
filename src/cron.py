c4817a8842c727fb6bf0395155fb8902701b9e24
Diego De la Vega <diego.delavega@crehana.com>
2024-03-28 01:24:24 -0500

    Tests1

--- src/cron.py
+++ src/cron.py
@@ -1,9 +1,12 @@
+import json
+from datetime import datetime
+
 
 def handler(event, context):
-    print("event", event)
-    print("context", context)
+    current_time = datetime.now().isoformat()
+    print(f"The current time is {current_time}")
     return {
         'statusCode': 200,
-        'body': 'Ejecución exitosa'
+        'body': json.dumps(f"The current time is {current_time}")
     }
 
