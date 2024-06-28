b7d613335e02f031d76da7906fbcf98e02041bc9
Diego De la Vega <diego.delavega@crehana.com>
2024-06-27 22:58:48 -0500

    Feat: Addin activate reinvestment

--- src/functions.py
+++ src/functions.py
@@ -1,3 +1,4 @@
+from src.integrations.sengrid import SendgridMail
 from src.services import PreInvestment, Reinvestment, Investment
 import logging
 import uuid
@@ -20,25 +21,86 @@ def activate_re_investments(event, context):
     try:
         list_re_investments = Reinvestment.execute_get_end_reinvestment(100)
         print("list_re_investments", list_re_investments)
-        for re_investment in list_re_investments:
-            Reinvestment.execute_update_active_re_investment(re_investment['uuid'], True)
-            Reinvestment.execute_activate_pre_investment(re_investment['new_pre_investment_id'])
-            __operation_code = Investment.execute_get_last_investments(1)[0]['operation_code']
-            logger.info("***__operation_code", __operation_code)
-            prefix = "INV"
-            number = int(__operation_code[3:]) + 1
-            new_operation_code = f"{prefix}{number:04d}"
-            Investment.execute_create_investment(
-                str(uuid.uuid4()),
-                new_operation_code,
-                re_investment['new_pre_investment_id'],
-                re_investment['start_re_investment'],
-                re_investment['end_re_investment'],
-                datetime.now(timezone.utc),
-            )
+        list_new_pre_investments_uuids = []
+        to_emails_fields = []
+        if list_re_investments:
+            for re_investment in list_re_investments:
+                Reinvestment.execute_update_active_re_investment(re_investment['uuid'], True)
+                Reinvestment.execute_activate_pre_investment(re_investment['new_pre_investment_id'])
+                __operation_code = Investment.execute_get_last_investments(1)[0]['operation_code']
+                logger.info("***__operation_code", __operation_code)
+                prefix = "INV"
+                number = int(__operation_code[3:]) + 1
+                new_operation_code = f"{prefix}{number:04d}"
+                Investment.execute_create_investment(
+                    str(uuid.uuid4()),
+                    new_operation_code,
+                    re_investment['new_pre_investment_id'],
+                    re_investment['start_re_investment'],
+                    re_investment['end_re_investment'],
+                    datetime.now(timezone.utc),
+                )
+                list_new_pre_investments_uuids.append(re_investment['new_pre_investment_id'])
+            list_new_pre_investments = PreInvestment.execute_get_list_pre_investment(list_new_pre_investments_uuids)
+            print("list_new_pre_investments", list_new_pre_investments)
+
+            for investment in list_new_pre_investments:
+                currency_symbol = "S/. " if investment['currency'] == 'nuevo sol' else "$ "
+                mailer_template_data = {
+                    "email": investment['email'],
+                    "fields": {
+                        "full_name": investment['first_name'] + " " + investment['last_name'],
+                        "amount": currency_symbol + str(investment['amount']),
+                        "deadline": str(investment['deadline_value']) + " meses",
+                        "investment_start_date": datetime.now(timezone.utc).strftime("%d/%m/%Y"),
+                        "rentability": str(investment['profitability_percent']),
+                    }
+                }
+                to_emails_fields.append(mailer_template_data)
+        params = {
+            "template_id": "d-d543e94efac241f9b3cf317840d48492",
+            "subject": "Re Investment Register",
+        }
+        init_mails = SendgridMail()
+        print("to_emails_fields", to_emails_fields)
+        sender_emails = init_mails.send_email(to_emails_fields, params.get("subject"), params.get("template_id"))
+        if sender_emails.status_code == 202:
+            print("sender_emails.status_code", sender_emails.status_code)
         logger.info("***list_re_investments", list_re_investments)
         success = True
     except Exception as e:
         logger.exception("Failed Lambda ***activate_re_investments", e)
     return success
 
+def send_mail_reinvestment(event, context):
+
+    try:
+        to_emails_fields = [
+            {
+                "email": 'ddvloayza@gmail.com',
+                "fields": {
+                    "full_name": 'Diego De la vega',
+                    "plan_name": "plan de prueba",
+                    "amount": 1000,
+                    "currency": 'Nuevos Soles',
+                    "status": 'Pendiente de aprobacion',
+                },
+            }
+        ]
+        params = {
+            "template_id": "d-9c312be3b63c4677adc48996bde407aa",
+            "subject": "Re Investment Register",
+        }
+        init_mails = SendgridMail()
+        sender_emails = init_mails.send_email(to_emails_fields, params.get("subject"), params.get("template_id"))
+        if sender_emails.status_code == 202:
+            success_send_mail = True
+    except Exception as e:
+        print("**ERROR**", str(e))
+        logger.info("Error in send_mail", str(e))
+
+
+if __name__ == "__main__":
+    event = {}
+    context = {}
+    activate_re_investments(event, context)
--- src/queries.py
+++ src/queries.py
@@ -15,7 +15,8 @@ def query_get_pre_investments(status, limit):
             investment_preinvestment.deadline_id,
             investment_preinvestment.coupon_id,
             investment_preinvestment.currency,
-            investment_preinvestment.created_at
+            investment_preinvestment.created_at,
+            investment_preinvestment.status
         FROM investment_preinvestment
         WHERE investment_preinvestment.status = '{status}'
         LIMIT {limit}
@@ -38,3 +39,39 @@ def query_create_disable_preinvestment(
         ) VALUES {', '.join(data)}
     """
 
+def query_get_list_pre_investments(uuid_str):
+    print("uuid_str", uuid_str)
+    return f"""
+        SELECT
+          "public"."investment_preinvestment"."uuid" AS "uuid",
+          "public"."investment_preinvestment"."created_at" AS "created_at",
+          "public"."investment_preinvestment"."updated_at" AS "updated_at",
+          "public"."investment_preinvestment"."is_active" AS "is_active",
+          "public"."investment_preinvestment"."amount" AS "amount",
+          "public"."investment_preinvestment"."is_investment" AS "is_investment",
+          "public"."investment_preinvestment"."currency" AS "currency",
+          "public"."investment_preinvestment"."operation_code" AS "operation_code",
+          "public"."investment_preinvestment"."action_status" AS "action_status",
+          "Onboarding Plan - Plan"."value" AS "plan_value",
+          "Investment Coupon - Coupon"."discount_percent" AS "coupon_discount",
+          "Onboarding Deadline - Deadline"."value" AS "deadline_value",
+          "Customuser Customuser - User"."email" AS "email",
+          "Customuser Userprofile - User"."first_name" AS "first_name",
+          "Customuser Userprofile - User"."last_name" AS "last_name",
+          CASE
+            WHEN "Onboarding Deadline - Deadline"."value" = 6 THEN "Onboarding Plan - Plan"."six_months_return"
+            WHEN "Onboarding Deadline - Deadline"."value" = 12 THEN "Onboarding Plan - Plan"."twelve_months_return"
+            WHEN "Onboarding Deadline - Deadline"."value" = 24 THEN "Onboarding Plan - Plan"."return_twenty_four_months"
+            WHEN "Onboarding Deadline - Deadline"."value" = 36 THEN "Onboarding Plan - Plan"."return_thirty_six_months"
+            WHEN "Onboarding Deadline - Deadline"."value" = 48 THEN "Onboarding Plan - Plan"."return_forty_eight_months"
+            ELSE NULL
+          END AS "profitability_percent"
+        FROM
+          "public"."investment_preinvestment"
+        LEFT JOIN "public"."onboarding_plan" AS "Onboarding Plan - Plan" ON "public"."investment_preinvestment"."plan_id" = "Onboarding Plan - Plan"."uuid"
+        LEFT JOIN "public"."investment_coupon" AS "Investment Coupon - Coupon" ON "public"."investment_preinvestment"."coupon_id" = "Investment Coupon - Coupon"."uuid"
+        LEFT JOIN "public"."onboarding_deadline" AS "Onboarding Deadline - Deadline" ON "public"."investment_preinvestment"."deadline_id" = "Onboarding Deadline - Deadline"."uuid"
+        LEFT JOIN "public"."customuser_customuser" AS "Customuser Customuser - User" ON "public"."investment_preinvestment"."user_id" = "Customuser Customuser - User"."id"
+        LEFT JOIN "public"."customuser_userprofile" AS "Customuser Userprofile - User" ON "public"."investment_preinvestment"."user_id" = "Customuser Userprofile - User"."user_id"
+        WHERE investment_preinvestment.uuid IN ({uuid_str});
+    """
